69f19526f1e499d008775db5678b9f218885962d
Mathis Pinsault <mathis.pinsault@epitech.eu>
2020-06-08 19:05:08 +0200

    Add profile route that check token and send profile infos to user

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const signin = require('./signin');
 const signup = require('./signup');
 const validate = require( './validate' );
+const jwt = require('jsonwebtoken');
 
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
@@ -17,5 +18,42 @@ router.post("/signin", (req, res, next) => {
 router.get("/validation/:id", (req, res, next) => {
     validate(req, res, next);
 })
+
+
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+    console.log(req.headers);
+
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        console.log('Forbidden')
+        res.sendStatus(403)
+    }
+}
+
+router.get('/profile', checkToken, (req, res) => {
+    //verify the JWT token generated for the user
+    jwt.verify(req.token, process.env.TOKEN_SECRET, (err, authorizedData) => {
+        if(err){
+            //If error send Forbidden (403)
+            console.log('ERROR: Could not connect to the protected route');
+            res.sendStatus(403);
+        } else {
+            //If token is successfully verified, we can send the autorized data 
+            authorizedData.user.password = null;
+            res.status(200).send({
+                message: 'Successful log in',
+                authorizedData
+            });
+            console.log('SUCCESS: Connected to protected route');
+        }
+    })
+});
 
 module.exports = router;
